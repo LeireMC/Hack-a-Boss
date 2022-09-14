f66d5bf98da488a46d1fb5cc3b5c1252f55b9870
Leire Martin <leiremc.03@gmail.com>
2022-09-14 18:06:00 +0200

    main restaurado

--- Backend/helpers.js
+++ Backend/helpers.js
@@ -1,14 +1,3 @@
-const { unlink } = require('fs/promises');
-const path = require('path'); // dependencia que nos sirve para crear rutas absolutas
-/* const sharp = require('sharp');
-const uuid = require('uuid'); */
-
-// Creamos la ruta absoluta a la carpeta de avatares
-const avatarsDir = path.join(__dirname, 'static/avatars');
-
-//Creamos la ruta absoluta a la carpeta de productos
-const postsDir = path.join(__dirname, 'static/products');
-
 //Función que genera un error
 function generateError(message, code) {
     const error = new Error(message);
@@ -16,37 +5,6 @@ function generateError(message, code) {
     return error;
 }
 
-//Función que valida un schema
-async function validate(schema, data) {
-    try {
-        await schema.validateAsync(data);
-    } catch (error) {
-        error.httpStatus = 400;
-        throw error;
-    }
-}
-
-//Función para borrar imágenes
-async function deletePhoto(photoName, type) {
-    try {
-        let photoPath;
-
-        if (type === 0) {
-            photoPath = path.join(avatarsDir, photoName);
-        } else if (type === 1) {
-            photoPath = path.join(postsDir, photoName);
-        }
-
-        await unlink(photoPath);
-    } catch (error) {
-        throw new Error(
-            'Se ha producido un error al eliminar la imagen del servidor. Por favor intenta de nuevo.'
-        );
-    }
-}
-
 module.exports = {
     generateError,
-    deletePhoto,
-    validate,
 };
