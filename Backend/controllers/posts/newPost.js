ed7d9552b82cdc7a5dfcf6e4365028ddb8a8bc34
Leire Martin <leiremc.03@gmail.com>
2022-09-14 17:03:48 +0200

    falta la cansulta para añadir la foto al post en el controller newPost

--- Backend/controllers/posts/newPost.js
+++ Backend/controllers/posts/newPost.js
@@ -1,4 +1,4 @@
-const validate = require('../../helpers');
+const { validate, generateError } = require('../../helpers');
 const getDB = require('../../db/getDB');
 const { newPostSchema } = require('../../schemas/newPostSchema');
 
@@ -8,10 +8,33 @@ const newPost = async (req, res, next) => {
         connection = await getDB();
 
         //Recuperamos los datos del body de la request
-        const { authorComments, hastagsh } = req.body;
+        const { authorComment, hashtag } = req.body;
 
         //Validar los datos recibidos por el body
         await validate(newPostSchema, req.body);
+
+        if (!authorComment) {
+            throw generateError(
+                'Debes escribir un comentario sobre tu fotografía',
+                400
+            );
+        }
+
+        //Recuperamos el id del usuario
+        const idReqUser = req.userAuth.id;
+
+        //Si nos indica el comentario, insertamos los datos en la base de datos
+        const [{ insertId }] = await connection.query(
+            `INSERT INTO post (authorComment, hashtag, createdAt, idUser)
+            VALUES (?, ?, ?, ?, ?)`,
+            [authorComment, hashtag, new Date(), idReqUser]
+        );
+
+        res.send({
+            status: 'Ok',
+            message: 'Post creado con éxito!',
+            data: { id: insertId, authorComment, hashtag },
+        });
     } catch (error) {
         next(error);
     } finally {
