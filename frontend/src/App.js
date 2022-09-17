09006c77091d558c9fd0dc181912422a5c5218d4
Leire Martin <leiremc.03@gmail.com>
2022-09-17 12:24:48 +0200

    rutas creadas en App.js. Archivos de paginas creadas con sus respectivos index.js y styles.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,29 @@
 import "./App.css";
-import { BrowserRouter /*  Routes, Route */ } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+import PostPage from "./pages/PostPage";
+import NewPostPage from "./pages/NewPostPage";
+import ProfilePage from "./pages/ProfilePage";
+import EditUserProfilePage from "./pages/EditUserProfilePage";
+import FavoritesPage from "./pages/FavoritesPage";
+import FollowingPage from "./pages/FollowingPage";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <MainPage>
-        <LoginPage></LoginPage>
-
-        <RegisterPage></RegisterPage>
-
-        <HomePage>
-          <Header></Header>
-
-          <Main></Main>
-
-          <Footer></Footer>
-        </HomePage>
-      </MainPage> */}
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/post/:idPost" element={<PostPage />} />
+        <Route path="/post/new" element={<NewPostPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/editUserProfile" element={<EditUserProfilePage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/following" element={<FollowingPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
